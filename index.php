cfef127c20031ebebc3acc3ff3d07dda57f1fdd8
greenLeradounaud <greenleradounaud@gmail.com>
2018-03-12 11:12:04 +0100

    vue debut

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-<?php include 'header.php' ?>
+<?php include 'includes/header.php' ?>
 <title>We_Transfert</title>
 </head>
 <body>
 
   <!-- Trigger the modal with a button -->
-  <button type="button" class="col-4 offset-2 btn btn-info btn-lg" data-toggle="modal" data-target="#inscription">Inscription</button>
-  <button type="button" class="col-4  btn btn-primary btn-lg" data-toggle="modal" data-target="#connexion">Log In</button>
+  <button type="button" class="col-4 offset-2 btn btn-info btn-lg" data-toggle="modal" data-target="#inscriptionmodal">Inscription</button>
+  <button type="button" class="col-4  btn btn-primary btn-lg" data-toggle="modal" data-target="#connexionmodal">Log In</button>
 
 
   <!-- Modal inscription-->
-  <div id="inscription" class="modal fade" role="dialog">
+  <div id="inscriptionmodal" class="modal fade" role="dialog">
     <div class="modal-dialog">
 
       <!-- Modal content-->
@@ -19,9 +19,11 @@
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body">
-         <input class="text-center col-4 offset-4" type="text" name="" placeholder="Nom">
-         <input class="text-center col-4 offset-4" type="text" name="" placeholder="Mail">
-         <input class="text-center col-4 offset-4" type="text" name="" placeholder="Password">
+         <form class=""  method="post">
+           <input class="text-center col-4 offset-4" type="text" name="nom" placeholder="Nom">
+           <input class="text-center col-4 offset-4" type="text" name="mail" placeholder="Mail">
+           <input class="text-center col-4 offset-4" type="text" name="password" placeholder="Password">
+         </form>
          <a href="#" class="text-center col-4 offset-4">Oups Mot de passe perdu?</a>
         </div>
         <div class="modal-footer">
@@ -31,7 +33,7 @@
     </div>
   </div>
   <!-- Modal connexion -->
-  <div id="connexion" class="modal fade" role="dialog">
+  <div id="connexionmodal" class="modal fade" role="dialog">
     <div class="modal-dialog">
 
       <!-- Modal content-->
@@ -41,8 +43,10 @@
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body">
-         <input class="text-center col-4 offset-4" type="text" name="" placeholder="mail">
-         <input class="text-center col-4 offset-4" type="text" name="" placeholder="password">
+         <form class=""  method="post">
+           <input class="text-center col-4 offset-4" type="text" name="mail" placeholder="mail">
+           <input class="text-center col-4 offset-4" type="text" name="password" placeholder="password">
+         </form>
         </div>
         <div class="modal-footer">
           <button type="button" class="col-4 btn-success btn btn-default">Connexion</button>
@@ -50,6 +54,12 @@
       </div>
     </div>
   </div>
+    <button type="button" class="col-4 offset-4 btn-success btn btn-default">Upload</button>
+    <p class="col-4 offset-4 text-center">Lien:</p>
+  </div>
+  <div class="">
+     <p class="text-center col-4 offset-4">Liste de vos fichiers:</p>
+  </div>
 
 
-<?php include 'footer.php' ?>
+<?php include 'includes/footer.php' ?>

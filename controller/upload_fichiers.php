23e3aba3b701f4ee069c141207b44e71fd1b327a
magentall <magentallteller@gmail.com>
2018-03-12 13:53:24 +0100

    sza12.5

--- controller/upload_fichiers.php
+++ controller/upload_fichiers.php
@@ -1,15 +1,15 @@
 <?php
-
+  include 'db/gestionDB.php';
 // check si l'utilisateur est log
 if ($_POST["id"]==null) {
   # utilisateur non connecté
   $lien=fct_insert_file($_POST["monFichier"],1);
-  $('#lien').html($lien);
+  //$('#lien').html($lien);
 }
 else {
   # utilisateur connecté
   $lien=fct_insert_file($_POST["monFichier"],$_POST["id"]);
-  $('#lien').html($lien);
-}html
+  //$('#lien').html($lien);
+}
 
  ?>

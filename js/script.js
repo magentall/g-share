3056cb82c40b41bbc8987a88f4287f565857e5a4
magentall <magentallteller@gmail.com>
2018-03-12 14:16:26 +0100

    sza12.6

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Ouverture de la page
+$(document).ready(function(){
+
+});
+
+function upload_fichiers(){
+  //var cat = $('#sel_cat option:selected').text();
+  $.ajax({
+    url:'controller/upload_fichiers.php',
+    data: { idf:$('#idf').val()
+    },
+    type: 'post',
+    success:function(output){
+      $('#lien').html(output);
+    }
+  });
+}
+/*
+
+function upload_fichiers_connec(){
+  //var cat = $('#sel_cat option:selected').text();
+  $.ajax({
+    url:'controller/upload_fichiers.php',
+    data: { cat:$('#sel_cat option:selected').text(),
+            aut:$('#sel_aut option:selected').text(),
+            filt:$('#sel_filt option:selected').text()
+    },
+    type: 'post',
+    success:function(output){
+      $('#lien').html(output);
+    }
+  });
+}
+*/

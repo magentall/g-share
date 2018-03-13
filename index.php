39aed14d09236217b53644a02ef78b7abb8251fc
LyraKuroneko <indigo.laetitia@gmail.com>
2018-03-13 16:04:42 +0100

    form

--- index.php
+++ index.php
@@ -6,40 +6,51 @@
 
 
 ?>
-<title>We_Transfert</title>
-</head>
-<body>
+        <title>We_Transfert</title>
+    </head>
+    <body>
 
-  <!-- Trigger the modal with a button -->
-  <div class="col-4 offset-4 text-center" id="title">
-    Bienvenue sur Gourou Uploads vous avez la possibilité d'uploader n'importe quel type de fichier (taille maximale : 3Mo) et obtenir en retour un lien pouvant être partagé et qui permet de downloader le fichier en question.<br><br> Un fichier uploadé est supprimé 10 minutes après son upload. Donc le lien ne serat plus valide.<br><br>
-    Vous avez la possibilité de vous inscrire, de vous connecter et de vous déconnecter.<br><br> Un utilisateur connecté peut uploader des fichiers de 7Mo max, les durées de vie des fichiers passe à 24h.<br><br> N'hesitez pas à nous rejoindre <span class="argent">(pour seulement 5.000€/mois)</span> afin de trouver spritualité et bonheur.<br><br> Gouroux Mac nous laveras de nos pechés et nous accompagneras dans l'au dela.<br><br> Gloire a Gourou Mac notre maitre spirituel a tous.
-  </div>
+        <?php
+            if(isset($_POST['err'])){
+              echo "Mot de passe erroné";
+            }
 
-  <button id="login" type="button" class="hover col-4 offset-4  btn btn-primary" data-toggle="modal" data-target="#connexionmodal">Log In</button>
-  <?php
-  include 'includes/ModalInscription.php';
-  include 'includes/ModalConnexion.php';
-  ?>
+            if(isset($_POST['id'])){
+              $_SESSION["id"] = $_POST['id'];
+              echo "salut";
+            }
+        ?>
 
-  <div class="">
-    <form id="form" action="ajaxupload.php" method="post" enctype="multipart/form-data">
-  		<input id="upload" class="browse col-4 offset-4 text-center" type="file"  name="file" />
-  		<button  id="button" class="col-4 offset-4 btn-success btn btn-default" type="submit" value="Upload">Upload</button>
-  	</form>
-  </div>
-  <div class="">
-        <span class="lien col-4 offset-4 text-center" >Votre Lien:
-            <a href="#" target="_blank" class="text-center" id="lien"></a>
-        </span>
-  </div>
-  <div class="">
-      <span class="text-center col-4 offset-4" id="fichiers">Liste de vos fichiers:</span>
-  </div>
+        <!-- Trigger the modal with a button -->
+        <div class="col-4 offset-4 text-center" id="title">
+          Bienvenue sur Gourou Uploads vous avez la possibilité d'uploader n'importe quel type de fichier (taille maximale : 3Mo) et obtenir en retour un lien pouvant être partagé et qui permet de downloader le fichier en question.<br><br> Un fichier uploadé est supprimé 10 minutes après son upload. Donc le lien ne serat plus valide.<br><br>
+          Vous avez la possibilité de vous inscrire, de vous connecter et de vous déconnecter.<br><br> Un utilisateur connecté peut uploader des fichiers de 7Mo max, les durées de vie des fichiers passe à 24h.<br><br> N'hesitez pas à nous rejoindre <span class="argent">(pour seulement 5.000€/mois)</span> afin de trouver spritualité et bonheur.<br><br> Gouroux Mac nous laveras de nos pechés et nous accompagneras dans l'au dela.<br><br> Gloire a Gourou Mac notre maitre spirituel a tous.
+        </div>
 
-<div id="err" >
+        <button id="login" type="button" class="hover col-4 offset-4  btn btn-primary" data-toggle="modal" data-target="#connexionmodal">Log In</button>
+        <?php
+        include 'includes/ModalInscription.php';
+        include 'includes/ModalConnexion.php';
+        ?>
 
-</div>
+        <div class="">
+          <form id="form" action="ajaxupload.php" method="post" enctype="multipart/form-data">
+            <input id="upload" class="browse col-4 offset-4 text-center" type="file"  name="file" />
+            <button  id="button" class="col-4 offset-4 btn-success btn btn-default" type="submit" value="Upload">Upload</button>
+          </form>
+        </div>
+        <div class="">
+              <span class="lien col-4 offset-4 text-center" >Votre Lien:
+                  <a href="#" target="_blank" class="text-center" id="lien"></a>
+              </span>
+        </div>
+        <div class="">
+            <span class="text-center col-4 offset-4" id="fichiers">Liste de vos fichiers:</span>
+        </div>
+
+      <div id="err" >
+
+      </div>
 
 
 

f08ef8dea2d3fdfcabb60ed5a908c5c0f877f246
greenLeradounaud <greenleradounaud@gmail.com>
2018-03-13 12:18:57 +0100

    pour toi grand gourou mac

--- ajaxupload.php
+++ ajaxupload.php
@@ -1,40 +1,26 @@
 <?php
 
-include 'db/connectDB.php';
-include 'db/gestionDB.php';
 
-//$valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp'); // valid extensions
-$path = 'uploads/'; // upload directory
 
-if(isset($_FILES['file']))
-{
-	$img = $_FILES['file']['name'];
-	$tmp = $_FILES['file']['tmp_name'];
+	include 'db/connectDB.php';
 
-	// get uploaded file's extension
-	$ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
+	include 'db/gestionDB.php';
 
-	// can upload same image using rand function
-	$final_image = rand(1000,1000000).$img;
 
-	// check's valid format
-	//if(in_array($ext, $valid_extensions))
-	//{
-		$path = $path.strtolower($final_image);
 
-		if(move_uploaded_file($tmp,$path))
-		{
-			//echo "<img src='$path' />";
-			echo fct_insert_file($path, 1);
-			/*echo "<p>
-			".$path."
-			</p>";*/
-		}
-	else
+	$path = 'uploads/'; // upload directory
+
+
+	if(isset($_FILES['file']))
 	{
-		echo 'invalid';
+		$file = $_FILES['file']['name'];
+		$tmp = $_FILES['file']['tmp_name'];
+		$id = $_POST["id"];
+		$id = 1;
+		$link =  fct_insert_file($file, $id);
+		$path = $path.strtolower($link);
+		move_uploaded_file($tmp, $path);
+		echo "<img src='$path' />";
+		echo "<a href=".$path.">Lien</a>";
 	}
-}
-
-
 ?>

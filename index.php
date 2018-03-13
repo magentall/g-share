d6473eeec8e41cebaaec60084567295661626174
greenLeradounaud <greenleradounaud@gmail.com>
2018-03-13 11:07:38 +0100

    guru mac nout guidera tous vers la liumiere

--- index.php
+++ index.php
@@ -16,8 +16,11 @@
   ?>
 
   <div class="">
-      <input class="browse col-4 offset-4 text-center" id="idf" type="file">
-      <button type="button" class="col-4 offset-4 btn-success btn btn-default" onclick="upload_fichiers()">Upload</button>
+    <form id="form" action="ajaxupload.php" method="post" enctype="multipart/form-data">
+  		<input id="uploadImage" class="browse col-4 offset-4 text-center" type="file" accept="image/*" name="file" />
+      <div class="col-4 offset-4" id="preview"><img src="no-image.jpg" /></div>
+  		<button  id="button" class="col-4 offset-4 btn-success btn btn-default" type="submit" value="Upload">Upload</button>
+  	</form>
   </div>
   <div class="">
       <span class="lien col-4 offset-4 text-center" >Votre Lien:<span class="text-center" id="lien"></span></span>
@@ -26,11 +29,8 @@
       <span class="text-center col-4 offset-4" id="fichiers">Liste de vos fichiers:</span>
   </div>
 
-  <div id="preview"><img src="no-image.jpg" /></div>
 
-  <form id="form" action="ajaxupload.php" method="post" enctype="multipart/form-data">
-		<input id="uploadImage" type="file" name="image" />
-		<input id="button" type="submit" value="Upload">
-	</form>
+
+
 
 <?php include 'includes/footer.php' ?>

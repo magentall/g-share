0e21916f3f5f650affa9e4f2ab0d32bf9beef51c
greenLeradounaud <greenleradounaud@gmail.com>
2018-03-13 11:45:03 +0100

    ds,kngjnseg ,fdjn,g dfskv,fdn ljf,dsf ln,fbn ldf

--- index.php
+++ index.php
@@ -8,11 +8,12 @@
 <body>
 
   <!-- Trigger the modal with a button -->
-  <div id="title">
-    Uploadez-vos fichiers
+  <div class="col-4 offset-4 text-center" id="title">
+    Bienvenue sur Gourou Uploads vous avez la possibilité d'uploader n'importe quel type de fichier (taille maximale : 3Mo) et obtenir en retour un lien pouvant être partagé et qui permet de downloader le fichier en question. Un fichier uploadé est supprimé 10 minutes après son upload. Donc le lien ne serat plus valide.
+    Vous avez la possibilité de vous inscrire, de vous connecter et de vous déconnecter. Un utilisateur connecté peut uploader des fichiers de 7Mo max, les durées de vie des fichiers passe à 24h. Un bouton don est a votre disposition afin de nous rejoindre et trouver spritualité et bonheur. Gouroux Mac nous laveras de nos pechés et nous accompagneras dans l'au dela. Gloire a Gourou Mac notre maitre spirituel a tous.
   </div>
 
-  <button id="login" type="button" class="col-4 offset-4  btn btn-primary btn-lg" data-toggle="modal" data-target="#connexionmodal">Log In</button>
+  <button id="login" type="button" class="hover col-4 offset-4  btn btn-primary btn-lg" data-toggle="modal" data-target="#connexionmodal">Log In</button>
   <?php
   include 'includes/ModalInscription.php';
   include 'includes/ModalConnexion.php';
@@ -21,7 +22,7 @@
   <div class="">
     <form id="form" action="ajaxupload.php" method="post" enctype="multipart/form-data">
   		<input id="uploadImage" class="browse col-4 offset-4 text-center" type="file" accept="image/*" name="file" />
-      <div class="col-4 offset-4" id="preview"><img src="no-image.jpg" /></div>
+      <a href="#"><div class="col-4 offset-4" id="preview"><img src="no-image.jpg" /></div></a>
   		<button  id="button" class="col-4 offset-4 btn-success btn btn-default" type="submit" value="Upload">Upload</button>
   	</form>
   </div>

26f2b8877b24bf63c2f0184a2fd1ff562e48e1c0
magentall <magentallteller@gmail.com>
2018-03-13 13:49:55 +0100

    sza13.11

--- ajaxupload.php
+++ ajaxupload.php
@@ -3,7 +3,7 @@
 include 'db/connectDB.php';
 include 'db/gestionDB.php';
 
-//$valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp'); // valid extensions
+
 $path = 'uploads/'; // upload directory
 
 if(isset($_FILES['file']))
@@ -13,33 +13,18 @@ if(isset($_FILES['file']))
 	$id = 1;
 
 
-
 	$link =  fct_insert_file($file, $id);
 	$path = $path.strtolower($link);
 
-	$path = 'http://'.$_SERVER['HTTP_HOST'].$path;
+	$linkup = 'http://'.$_SERVER['HTTP_HOST']."/".$path;
 
 
-/*
 
-	// get uploaded file's extension
-	$ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
-
-	// can upload same image using rand function
-	$final_image = rand(1000,1000000).$img;
-
-	// check's valid format
-	//if(in_array($ext, $valid_extensions))
-	//{
-		$path = $path.strtolower($final_image);
-*/
 		if(move_uploaded_file($tmp,$path))
 		{
-			//echo "<img src='$path' />";
-			echo $path;
-			/*echo "<p>
-			".$path."
-			</p>";*/
+
+			echo $linkup;
+
 		}
 	else
 	{
@@ -47,26 +32,4 @@ if(isset($_FILES['file']))
 	}
 }
 
-
-	include 'db/connectDB.php';
-
-	include 'db/gestionDB.php';
-
-
-
-	$path = 'uploads/'; // upload directory
-
-
-	if(isset($_FILES['file']))
-	{
-		$file = $_FILES['file']['name'];
-		$tmp = $_FILES['file']['tmp_name'];
-		$id = $_POST["id"];
-		$id = 1;
-		$link =  fct_insert_file($file, $id);
-		$path = $path.strtolower($link);
-		move_uploaded_file($tmp, $path);
-		echo "<img src='$path' />";
-		echo "<a href=".$path.">Lien</a>";
-	}
 ?>
